19b782b04c8108e5aeb2dc49826d4c73178b2b26
controller <adubey4@hawk.iit.edu>
2015-10-10 13:56:08 -0500

    Created launch.sh

---
+++ launch.sh
@@ -0,0 +1,2 @@
+
+aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name adubey04 --security-group-ids sg-3df3e75a --subnet-id subnet-38446a61 --associate-public-ip-address --user-data file://../itmo-544-444-env/install-env.sh --debug
